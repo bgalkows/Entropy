8de4fb49934c9d5e85307242ae4c7017b5668841
TimidJay <justizl1@uci.edu>
2017-01-14 00:44:07 -0800

    Adjusted scripts

--- Thingy/Assets/Bullet.cs
+++ Thingy/Assets/Bullet.cs
@@ -9,13 +9,13 @@ public class Bullet : MonoBehaviour {
     private bool isAimed;
     //Spiral shots will use direction, aimed shots will use the AwayFromTargets
     //The AwayFromTarget variables I used in my earlier game were the difference between the bullet's x and y and the target's x and y
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
         if (!isAimed)
         {
             position = new Vector2(position.x + (speed * Mathf.Cos(Mathf.Deg2Rad * direction)), position.y - (speed * Mathf.Sin(Mathf.Deg2Rad * direction)));
@@ -24,16 +24,14 @@ public class Bullet : MonoBehaviour {
         {
             position = new Vector2(position.x + (speed * Mathf.Cos(Mathf.Deg2Rad * (Mathf.Atan2(xAwayFromTarget, yAwayFromTarget) - 90))), position.y - (speed * Mathf.Cos(Mathf.Deg2Rad * (Mathf.Atan2(xAwayFromTarget, yAwayFromTarget) - 90))));
         }
-        hitCheck(player);
-	}   
+    }
 
-    public void SpawnDirectional(float x,float y,float s, float dir)
+    public void SpawnDirectional(float x, float y, float s, float dir)
     {
         speed = s;
         direction = dir;
         isAimed = false;
         position = new Vector2(x, y);
-        //Instantiate(shot, new Vector2(x, y), Quaternion.identity); This should probably go in a bullet spawner script
     }
 
     public void SpawnAimed(float x, float y, float s, float xAway, float yAway)
@@ -45,7 +43,18 @@ public class Bullet : MonoBehaviour {
         position = new Vector2(x, y);
     }
 
-    void hitCheck(GameObject player)
+    void onCollisionEnter(Collision c)
     {
+        if (c.gameObject.name == "Player")
+        {
+            //make the function for taking damage public in the player class
+            Destroy(this, .5f);
+        }
     }
+    public float getSpeed() { return speed; }
+    public float getDir() { return direction; }
+    public float getXDiff() { return xAwayFromTarget; }
+    public float getYDiff() { return yAwayFromTarget; }
+    public bool getAimed() { return isAimed; }
+    public Vector2 getPos() { return position; }
 }
