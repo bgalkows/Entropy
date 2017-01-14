36a424eb15fd1f12197d8864c61cef03c6610aa8
Brett Galkowski <bgalkows@uci.edu>
2017-01-14 11:28:13 -0800

    boss health creation

--- Thingy/Assets/Scripts/Player.cs
+++ Thingy/Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
-    public float speed = 2;
+    public float speed = 4;
     public int lives;
     public int currentHealth;
     public bool invincible;
