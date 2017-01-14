aab7074c0bc0c43f7868c2fa123ab072d6ded3f1
Brett Galkowski <bgalkows@uci.edu>
2017-01-14 01:14:21 -0800

    Player Movement
    lots of player class additions

--- Thingy/Assets/Player.cs
+++ Thingy/Assets/Player.cs
@@ -3,14 +3,115 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
+    public float speed = 2;
+    public int lives;
+    public int currentHealth;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake()
+    {
+        lives = 3;
+        currentHealth = 100; 
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update() {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Debug.Log("Move right");
+                this.transform.Translate((speed / 2) * Time.deltaTime, 0, 0);
+            }
+            else if ( Input.GetKey(KeyCode.LeftArrow))
+            {
+                Debug.Log("Move left");
+                this.transform.Translate((-speed / 2) * Time.deltaTime, 0, 0);
+            }
+            else if ( Input.GetKey(KeyCode.DownArrow))
+            {
+                Debug.Log("Move down");
+                this.transform.Translate(0, (-speed / 2) * Time.deltaTime, 0);
+            }
+            else if ( Input.GetKey(KeyCode.UpArrow))
+            {
+                Debug.Log("Move up");
+                this.transform.Translate(0, (speed / 2) * Time.deltaTime, 0);
+            }
+        }
+        else
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                Debug.Log("Move right");
+                this.transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.LeftArrow))
+            {
+                Debug.Log("Move left");
+                this.transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.DownArrow))
+            {
+                Debug.Log("Move down");
+                this.transform.Translate(0, -speed  * Time.deltaTime, 0);
+            }
+            else if (Input.GetKey(KeyCode.UpArrow))
+            {
+                Debug.Log("Move up");
+                this.transform.Translate(0, speed  * Time.deltaTime, 0);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.Z))
+        {
+            Debug.Log("Z press");
+            //shooting here later
+        }
+       
 	}
+
+    public void setPosition(float x, float y)
+    {
+        this.transform.position = new Vector2(x, y);
+    }
+
+    public void Respawn()
+    {
+        currentHealth = 100;
+        Debug.Log("You respawned with 100 health.");
+        setPosition(0, 0);
+    }
+
+
+    public void Death()
+    {
+        if (lives > 0)
+        {
+            Respawn();
+            lives--;
+            Debug.Log(lives + " lives left!");
+        }
+        else
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        Debug.Log("GAME OVER");
+        //SceneManager.LoadScene("GameOverScene");
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        Debug.Log("Player lost" + amount + "health, " + currentHealth + "remaining.");
+
+        if(currentHealth<=0)
+        {
+            Death();
+        }
+    }
 }
